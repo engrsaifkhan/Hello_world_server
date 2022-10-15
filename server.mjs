bd7019aec10404296f692211a6d2ef69fd3d8df7
Sufy97 <saifkhan007ii@gmail.com>
2022-10-16 01:50:14 +0500

    changes made

--- server.mjs
+++ server.mjs
@@ -1,11 +1,31 @@
 import express from 'express'
+import cors from 'cors'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.use(cors())
+app.get('/home', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/profile', (req, res) => {
+  res.send('I am profile page')
+})
+
+app.get('/weather', (req, res) =>{
+  res.send({
+    temp_c:'28',
+    region:'sindh',
+    city: 'karachi',
+    localtime: '10:15',
+    condition:'partially cloud',
+    humidity: '11',
+    precip_mm: '0.5',
+    uv: '0.2',
+    vis_km: '130',
+    wind_kph: '15',
+  })
+})
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`local host: ${port}`)
 })
