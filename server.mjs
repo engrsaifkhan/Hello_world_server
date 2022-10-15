2ff28815c769e64ccbb3fb21451382a1592926d6
Sufy97 <saifkhan007ii@gmail.com>
2022-10-16 02:23:30 +0500

    port settings done

--- server.mjs
+++ server.mjs
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 const app = express()
-const port = 3000
+// const port = 3000;
 
 app.use(cors())
 app.get('/home', (req, res) => {
@@ -26,6 +26,9 @@ app.get('/weather', (req, res) =>{
     wind_kph: '15',
   })
 })
+
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`local host: ${port}`)
 })
